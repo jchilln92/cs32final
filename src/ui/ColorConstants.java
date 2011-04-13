7330be2d6fad1457f8e175f22acb1fcc35bafe2b
Joel <jnackman@cs.brown.edu>
2011-04-13 19:14:41 -0400

    Removed some fancier graphics drawing code that is now stored in the branch fancy_graphics.  We will work on this code later.  Added a few more class stubs.

--- src/ui/ColorConstants.java
+++ src/ui/ColorConstants.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 
 public class ColorConstants {
 	public static final Color pathColor = Color.decode("#F0C185");
-	public static final Color pathColor2 = Color.decode("#D6AA71");
 	public static final Color grassColor = Color.decode("#93C47D");
 	public static final Color rockColor = Color.decode("#8B847A");
 	public static final Color waterColor = Color.decode("#3D85C6"); 
--- src/ui/TileDrawer.java
+++ src/ui/TileDrawer.java
@@ -1,14 +1,6 @@
 package src.ui;
 
-import java.awt.Color;
-import java.awt.LinearGradientPaint;
 import java.awt.Graphics2D;
-import java.awt.MultipleGradientPaint;
-import java.awt.Paint;
-import java.awt.RadialGradientPaint;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Arc2D;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 import src.core.TileType;
@@ -29,71 +21,9 @@ public class TileDrawer {
 			g.fill(tile);
 			break;
 		case PATH:
-		case PATH_VERTICAL:
-		case PATH_HORIZONTAL:
-		case PATH_TURN_UP_RIGHT:
-		case PATH_TURN_UP_LEFT:
-		case PATH_TURN_DOWN_RIGHT:
-		case PATH_TURN_DOWN_LEFT:
-		case PATH_TURN_LEFT_UP:
-		case PATH_TURN_LEFT_DOWN:
-		case PATH_TURN_RIGHT_UP:
-		case PATH_TURN_RIGHT_DOWN:
-			paintPathTile(type, tile, g);
+			g.setPaint(ColorConstants.pathColor);
+			g.fill(tile);
 			break;
 		}
-	}
-	
-	public static void paintPathTile(TileType type, Rectangle2D tile, Graphics2D g) {
-		AffineTransform oldT = g.getTransform();
-		
-		// define the gradient used for straight path pieces - this one is the correct gradient for drawing
-		// vertical path pieces.  It will be rotated using an affine transform to make a horizontal tile
-		Point2D p1 = new Point2D.Double(tile.getX() - tile.getWidth() * .3, tile.getCenterY());
-		Point2D p2 = new Point2D.Double(tile.getX() + tile.getWidth() * 1.3, tile.getCenterY());
-		float[] dist = {.01f, .5f, .99f};
-		Color[] colors = {ColorConstants.pathColor, ColorConstants.pathColor2, ColorConstants.pathColor};
-		LinearGradientPaint straightPaint = new LinearGradientPaint(p1, p2, dist, colors);
-		
-		// define the radial gradient used for drawing curved path segments
-		double radius = tile.getWidth() * 1.3;
-		Point2D center = new Point2D.Double(tile.getX(), tile.getMaxY());
-		Point2D focus = new Point2D.Double(tile.getX() - (tile.getWidth() * .3)/Math.sqrt(2), 
-										   tile.getMaxY() - (tile.getWidth() * .3)/Math.sqrt(2));
-		RadialGradientPaint curvedPaint = new RadialGradientPaint(center, (float)radius, focus, 
-				dist, colors, MultipleGradientPaint.CycleMethod.NO_CYCLE);
-		
-		Arc2D sector = new Arc2D.Double();
-		sector.setArcByCenter(tile.getX(), tile.getMaxY(), tile.getWidth(), 0, 90, Arc2D.PIE);
-		
-		switch (type) {
-		case PATH_HORIZONTAL:
-			g.setPaint(straightPaint);
-			g.rotate(Math.PI/2, tile.getCenterX(), tile.getCenterY());
-			g.fill(tile);
-			break;
-		case PATH:
-		case PATH_VERTICAL:
-			g.setPaint(straightPaint);
-			g.fill(tile);
-			break;
-		case PATH_TURN_UP_RIGHT:
-			g.rotate(-Math.PI/2, tile.getCenterX(), tile.getCenterY());
-		case PATH_TURN_UP_LEFT:
-		case PATH_TURN_DOWN_LEFT:
-		case PATH_TURN_LEFT_UP:
-		case PATH_TURN_LEFT_DOWN:
-		case PATH_TURN_RIGHT_UP:
-		case PATH_TURN_RIGHT_DOWN:
-			g.rotate(Math.PI, tile.getCenterX(), tile.getCenterY());
-		case PATH_TURN_DOWN_RIGHT:
-			g.setPaint(ColorConstants.grassColor);
-			g.fill(tile);
-			g.setPaint(curvedPaint);
-			g.fill(sector);
-			break;
-		}
-		
-		g.setTransform(oldT);
 	}
 }
