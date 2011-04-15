fe738c33cf781e1b182d3c5e00e176b2c1ae89d5
Alec Lee <alec_lee@brown.edu>
2011-04-15 16:34:03 -0400

    created an Upgrade class

--- src/core/Upgrade.java
+++ src/core/Upgrade.java
@@ -1,23 +1,40 @@
 package src.core;
 
-
 /**
  * 
  * The upgrade class holds all of the changes a tower upgrade can apply onto a tower. 
  */
 public class Upgrade implements IPurchasable {
 	
-	private double instantDamageChange;
+	//All of these fields represent percentage increases that will be added on to a current tower's values 
+	//ex: an instantDamageChange value of 0.1 means a 10% increase in the towers instantDamage.
+	private double instantDamageChange; // modifies instantDamage
+	private double timeDamageChange;    // modifies time damage
+	private double periodChange;        // modifies period
+	private double effectDurationChange;// modifies effectDuration
+	private double speedEffectChange;   // modifies speedChange
+	
 	private double radiusChange;
-	private double investmentChange;
-	private double price;
-	//need to add fields for damage over time, changes to speed changes, etc
+	private double fireRateChange;
+	private boolean canHitFlying;
+	
+	private double priceOfUpgrade;
+
 	
 	public Upgrade(){
 		//Change later to use an XML reader to get these values
 		instantDamageChange = 0;
+		timeDamageChange = 0;
+		periodChange = 0;
+		effectDurationChange = 0;
+		speedEffectChange = 0;
+		
 		radiusChange = 0;
-		investmentChange = 0;
+		fireRateChange = 0;
+		canHitFlying = false;
+		
+		
+		priceOfUpgrade = 0;
 		this.setPrice(0);
 	}
 	
@@ -25,23 +42,78 @@ public class Upgrade implements IPurchasable {
 		return instantDamageChange;
 	}
 	
+	public double getTimeDamageChange(){
+		return timeDamageChange;
+	}
+	
+	public double periodChange(){
+		return periodChange;
+	}
+	
+	public double effectDurationChange(){
+		return effectDurationChange;
+	}
+	
+	public double speedEffectChange(){
+		return speedEffectChange;
+	}
+	
+	
 	public double getRadiusChange(){
 		return radiusChange;
 	}
 	
-	public double getInvestmentChange(){
-		return investmentChange;
+	public double getFireRateChange(){
+		return fireRateChange;
 	}
 
+	public boolean getHitsFlyingChange(){
+		return canHitFlying;
+	}
+	
 	@Override
 	public double getPrice() {
-		return price;
+		return priceOfUpgrade;
 	}
 
 	@Override
 	public void setPrice(double p) {
-		price = p;
+		priceOfUpgrade = p;
 	}
+	
+
+	//Modifies Damage d's fields based on this upgrades change fields
+	public void updateDamage(Damage d){
+		double curInstantDamage = d.getInstantDamage();
+		double curTimeDamage = d.getTimeDamage();
+		double curPeriod = d.getPeriod();
+		double curSpeedChange = d.getSpeedChange();
+		double curEffectDuration = d.getEffectDuration();
+		
+		
+		d.setInstantDamage(curInstantDamage + (curInstantDamage*instantDamageChange));
+		d.setTimeDamage(curTimeDamage + (curTimeDamage*timeDamageChange));
+		d.setPeriod(curPeriod + (curPeriod*periodChange));
+		d.setSpeedChange(curSpeedChange + (curSpeedChange*speedEffectChange));
+		d.setEffectDuration(curEffectDuration + (curEffectDuration*effectDurationChange));
+	}
+	
+	
+	public void updateTargeting(TargetingInfo t){
+		t.setHitsFlying(getHitsFlyingChange());
+	}
+	
+	public void updateTower(Tower t){
+		double curFireRate = t.getFireRate();
+		double curRadius = t.getRadius();
+		double curInvestment = t.getInvestment();
+		
+		t.setFireRate(curFireRate + curFireRate * getFireRateChange());
+		t.setRadius(curRadius + curRadius * getRadiusChange());
+		t.setInvestment(curInvestment + curInvestment * getRadiusChange());
+	}
+
+
 	
 	
 
