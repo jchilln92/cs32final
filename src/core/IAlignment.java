f196b984ae8f3dd5271bacd1c7f77d8566c6dfbc
Alec Lee <alec_lee@brown.edu>
2011-04-24 20:02:42 -0400

    the alignment interface (not implemented yet)

---
+++ src/core/IAlignment.java
@@ -0,0 +1,15 @@
+package src.core;
+/**
+ * An interface allowing for the setting and getting of a tower or creeps element
+ */
+public interface IAlignment {
+
+		public IAlignment.Alignment getAlignment();
+		public void setAlignment(IAlignment.Alignment alignment);
+		public enum Alignment{
+			NEUTRAL, BLUE, RED, YELLOW, GREEN;
+		}
+	
+
+	}
+
