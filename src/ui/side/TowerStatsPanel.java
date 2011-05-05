7188a7198f7215a201f69fb4fc2b0b9f216305b4
Alec Lee <alec_lee@brown.edu>
2011-05-05 17:14:27 -0400

    nothing really

--- src/ui/side/TowerStatsPanel.java
+++ src/ui/side/TowerStatsPanel.java
@@ -30,11 +30,15 @@ public class TowerStatsPanel extends JPanel {
 	private JLabel rangeLabel;
 	private JLabel abilitiesLabel;
 	private JLabel fireRateLabel;
+	private JLabel extraDamageLabel1; // extraDamageLabels used to represent slowing and over time damage
+	private JLabel extraDamageLabel2;
 	
 	private JLabel damageChangeLabel;
 	private JLabel rangeChangeLabel;
 	private JLabel rateChangeLabel;
 	private JLabel abilitiesChangeLabel;
+	private JLabel extraDamageChangeLabel1;
+	private JLabel extraDamageChangeLabel2;
 	
 	public TowerStatsPanel(GameController gc) {
 		super(new GridBagLayout());
@@ -49,11 +53,15 @@ public class TowerStatsPanel extends JPanel {
 		rangeLabel = new JLabel(" ");
 		abilitiesLabel = new JLabel(" ");
 		fireRateLabel = new JLabel(" ");
+		extraDamageLabel1 = new JLabel(" ");
+		extraDamageLabel2 = new JLabel(" ");
 		
 		damageChangeLabel = new JLabel(" ");
 		rangeChangeLabel = new JLabel(" ");
 		rateChangeLabel = new JLabel(" ");
 		abilitiesChangeLabel = new JLabel(" ");
+		extraDamageChangeLabel1 =  new JLabel(" ");
+		extraDamageChangeLabel2 = new JLabel(" ");
 		
 		GridBagConstraints c = new GridBagConstraints();
 		
@@ -95,6 +103,22 @@ public class TowerStatsPanel extends JPanel {
 		c.gridy = 4;
 		add(abilitiesChangeLabel, c);
 		
+		c.gridx= 0;
+		c.gridy = 5;
+		add(extraDamageLabel1, c);
+		
+		c.gridx = 1;
+		c.gridy = 5;
+		add(extraDamageChangeLabel1, c);
+		
+		c.gridx= 0;
+		c.gridy = 6;
+		add(extraDamageLabel2, c);
+		
+		c.gridx = 1;
+		c.gridy = 6;
+		add(extraDamageChangeLabel2, c);
+		
 	}
 	
 	public void setTower(Tower t) {
@@ -106,6 +130,8 @@ public class TowerStatsPanel extends JPanel {
 			rangeLabel.setText(" ");
 			fireRateLabel.setText(" ");
 			abilitiesLabel.setText(" ");
+			extraDamageLabel1.setText(" ");
+			extraDamageLabel2.setText(" ");
 		} else {
 			towerNameLabel.setText("Type: " + tower.getType().toString());
 			
@@ -115,6 +141,19 @@ public class TowerStatsPanel extends JPanel {
 			fireRateLabel.setText("Fire Rate: " + NumberFormat.getInstance().format(1/tower.getFirePeriod() * 1000/Runner.tickDuration) + " / s");
 			
 			setCurrentAbilities(tower);
+			
+			
+			//properly set the extra damage labels for slowing and damage over time
+			if (tower.getDamage().getSpeedChange() != 0) {
+				extraDamageLabel1.setText("Slow Effect: " + NumberFormat.getInstance().format(tower.getDamage().getSpeedChange() * 100) + "%");
+
+				if (tower.getDamage().getTimeDamage() != 0)
+					extraDamageLabel2.setText("DoT: " + NumberFormat.getInstance().format(tower.getDamage().getTimeDamage()));				
+				
+			} else if (tower.getDamage().getTimeDamage() != 0) {
+				extraDamageLabel1.setText("DoT: " + NumberFormat.getInstance().format(tower.getDamage().getTimeDamage()));		
+			}
+			
 		}
 	}
 	
@@ -127,6 +166,8 @@ public class TowerStatsPanel extends JPanel {
 			rateChangeLabel.setText(" ");
 			abilitiesChangeLabel.setText(" ");
 			controller.getSideBar().getPlayerStatsPanel().setGoldChange("");
+			extraDamageChangeLabel1.setText(" ");
+			extraDamageChangeLabel2.setText(" ");
 
 		} else {
 			String modifier = "";
@@ -174,7 +215,7 @@ public class TowerStatsPanel extends JPanel {
 			String newAbilities = "+ ";
 			
 			if (u.getHitsFlyingChange() && !tower.getTargeting().isHitsFlying())
-				newAbilities += "anti-air, ";
+				newAbilities += "Anti-air, ";
 			
 			//TODO: decide whether or not we want to add this in. As the game is built right now, it is
 			//		impossible for a tower to without damage over time or slowing to gain it as everything
@@ -189,6 +230,45 @@ public class TowerStatsPanel extends JPanel {
 			newAbilities = newAbilities.substring(0, newAbilities.length() -2);
 			abilitiesChangeLabel.setForeground(Color.GREEN);
 			abilitiesChangeLabel.setText(newAbilities);
+			
+			//For first extraDamageChangeLabel, check for slowing effect
+			if (upgrade.speedEffectChange() > 0) {
+				extraDamageChangeLabel1.setForeground(Color.GREEN);
+				modifier = " -";
+			} else {
+				extraDamageChangeLabel1.setForeground(Color.RED);
+				modifier = " +";
+			}
+			
+			if (upgrade.speedEffectChange() != 0)
+				extraDamageChangeLabel1.setText(modifier + Double.toString(upgrade.speedEffectChange() * 100) + "%");
+			
+			
+			//check for damage over time
+			if (upgrade.getTimeDamageChange() != 0) {
+				if (extraDamageChangeLabel1.getText().equals(" ")) {				
+					if (upgrade.getTimeDamageChange() > 0) {
+						extraDamageChangeLabel1.setForeground(Color.GREEN);
+						modifier = " +";
+					} else {
+						extraDamageChangeLabel1.setForeground(Color.RED);
+						modifier = " -";
+					}
+					
+					extraDamageChangeLabel1.setText(modifier + Double.toString(upgrade.getTimeDamageChange() * 100) + "%");
+				}
+				else {
+					if (upgrade.getTimeDamageChange() > 0) {
+						extraDamageChangeLabel2.setForeground(Color.GREEN);
+						modifier = " +";
+					} else {
+						extraDamageChangeLabel2.setForeground(Color.RED);
+						modifier = " -";
+					}
+					extraDamageChangeLabel2.setText(modifier + Double.toString(upgrade.getTimeDamageChange() * 100) + "%");
+				}
+			}
+
 		}
 	}
 	
@@ -196,16 +276,15 @@ public class TowerStatsPanel extends JPanel {
 	public void setCurrentAbilities(Tower tower){
 		String abilitiesText = "";
 		if (tower.getTargeting().isHitsFlying())
-			abilitiesText += "anti-air, ";
+			abilitiesText += "Anti-air, ";
 		if (tower.getDamage().getSpeedChange() != 0.0)
-			abilitiesText += "slowing, ";
+			abilitiesText += "Slowing, ";
 		if (tower.getDamage().getTimeDamage() != 0.0)
 			abilitiesText += "DoT, ";
 		
 		if (abilitiesText.length() > 0)
 			abilitiesText = abilitiesText.substring(0, abilitiesText.length() - 2);
 
-		
 		abilitiesLabel.setText("Abilities: " + abilitiesText);
 	}
 }
