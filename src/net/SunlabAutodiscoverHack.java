95aef7852da7d906502b7d0dd7ce61c0bfc20dcb
Joel Nackman <jnackman@cs.brown.edu>
2011-05-09 09:47:27 -0400

    stuff

--- src/net/SunlabAutodiscoverHack.java
+++ src/net/SunlabAutodiscoverHack.java
@@ -12,54 +12,31 @@ public class SunlabAutodiscoverHack {
 			addresses = new ArrayList<InetAddress>();
 
 			try {
-				/*addresses.add(InetAddress.getByName("cslab8a"));
-				addresses.add(InetAddress.getByName("cslab8b"));
-				addresses.add(InetAddress.getByName("cslab8c"));
-				addresses.add(InetAddress.getByName("cslab8d"));
-				addresses.add(InetAddress.getByName("cslab8e"));
-				addresses.add(InetAddress.getByName("cslab8f"));
-				addresses.add(InetAddress.getByName("cslab8g"));
-				addresses.add(InetAddress.getByName("cslab8h"));*/
-				addresses.add(InetAddress.getByName("cslab7a"));
-				addresses.add(InetAddress.getByName("cslab7b"));
-				addresses.add(InetAddress.getByName("cslab7c"));
-				addresses.add(InetAddress.getByName("cslab7d"));
-				addresses.add(InetAddress.getByName("cslab7e"));
-				addresses.add(InetAddress.getByName("cslab7f"));
-				addresses.add(InetAddress.getByName("cslab7g"));
-				addresses.add(InetAddress.getByName("cslab7h"));
-				addresses.add(InetAddress.getByName("cslab6a"));
-				addresses.add(InetAddress.getByName("cslab6b"));
-				addresses.add(InetAddress.getByName("cslab6c"));
-				addresses.add(InetAddress.getByName("cslab6d"));
-				addresses.add(InetAddress.getByName("cslab6e"));
-				addresses.add(InetAddress.getByName("cslab6f"));
-				addresses.add(InetAddress.getByName("cslab6g"));
-				addresses.add(InetAddress.getByName("cslab6h"));
-				addresses.add(InetAddress.getByName("cslab5a"));
-				addresses.add(InetAddress.getByName("cslab5b"));
-				addresses.add(InetAddress.getByName("cslab5c"));
-				addresses.add(InetAddress.getByName("cslab5d"));
-				addresses.add(InetAddress.getByName("cslab5e"));
-				addresses.add(InetAddress.getByName("cslab5f"));
-				addresses.add(InetAddress.getByName("cslab5g"));
-				addresses.add(InetAddress.getByName("cslab5h"));
-				addresses.add(InetAddress.getByName("cslab4a"));
-				addresses.add(InetAddress.getByName("cslab4b"));
-				addresses.add(InetAddress.getByName("cslab4c"));
-				addresses.add(InetAddress.getByName("cslab4d"));
-				addresses.add(InetAddress.getByName("cslab4e"));
-				addresses.add(InetAddress.getByName("cslab4f"));
-				addresses.add(InetAddress.getByName("cslab4g"));
-				addresses.add(InetAddress.getByName("cslab4h"));
-				/*addresses.add(InetAddress.getByName("cslab3a"));
+				addresses.add(InetAddress.getByName("cslab3a"));
 				addresses.add(InetAddress.getByName("cslab3b"));
 				addresses.add(InetAddress.getByName("cslab3c"));
 				addresses.add(InetAddress.getByName("cslab3d"));
 				addresses.add(InetAddress.getByName("cslab3e"));
 				addresses.add(InetAddress.getByName("cslab3f"));
 				addresses.add(InetAddress.getByName("cslab3g"));
-				addresses.add(InetAddress.getByName("cslab3h"));*/
+				addresses.add(InetAddress.getByName("cslab3h"));
+				addresses.add(InetAddress.getByName("cslab2a"));
+				addresses.add(InetAddress.getByName("cslab2b"));
+				addresses.add(InetAddress.getByName("cslab2c"));
+				addresses.add(InetAddress.getByName("cslab2d"));
+				addresses.add(InetAddress.getByName("cslab2e"));
+				addresses.add(InetAddress.getByName("cslab2f"));
+				addresses.add(InetAddress.getByName("cslab2g"));
+				addresses.add(InetAddress.getByName("cslab2h"));
+				addresses.add(InetAddress.getByName("cslab1a"));
+				addresses.add(InetAddress.getByName("cslab1b"));
+				addresses.add(InetAddress.getByName("cslab1c"));
+				addresses.add(InetAddress.getByName("cslab1d"));
+				addresses.add(InetAddress.getByName("cslab1e"));
+				addresses.add(InetAddress.getByName("cslab1f"));
+				addresses.add(InetAddress.getByName("cslab1g"));
+				addresses.add(InetAddress.getByName("cslab1h"));
+				addresses.add(InetAddress.getByName("cslab0a"));
 			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
