68855f98c430c3abb563579d7e6d1454d69415ef
Joel Nackman <jnackman@cs.brown.edu>
2011-05-13 19:19:04 -0400

    fix sunlab hack from presentation

--- src/net/SunlabAutodiscoverHack.java
+++ src/net/SunlabAutodiscoverHack.java
@@ -15,8 +15,30 @@ public class SunlabAutodiscoverHack {
 			addresses = new ArrayList<InetAddress>();
 
 			try {
-				addresses.add(InetAddress.getByName("cslab8c"));
-
+				addresses.add(InetAddress.getByName("cslab6a"));
+				addresses.add(InetAddress.getByName("cslab6b"));
+				addresses.add(InetAddress.getByName("cslab6c"));
+				addresses.add(InetAddress.getByName("cslab6d"));
+				addresses.add(InetAddress.getByName("cslab6e"));
+				addresses.add(InetAddress.getByName("cslab6f"));
+				addresses.add(InetAddress.getByName("cslab6g"));
+				addresses.add(InetAddress.getByName("cslab6h"));
+				addresses.add(InetAddress.getByName("cslab5a"));
+				addresses.add(InetAddress.getByName("cslab5b"));
+				addresses.add(InetAddress.getByName("cslab5c"));
+				addresses.add(InetAddress.getByName("cslab5d"));
+				addresses.add(InetAddress.getByName("cslab5e"));
+				addresses.add(InetAddress.getByName("cslab5f"));
+				addresses.add(InetAddress.getByName("cslab5g"));
+				addresses.add(InetAddress.getByName("cslab5h"));
+				addresses.add(InetAddress.getByName("cslab4a"));
+				addresses.add(InetAddress.getByName("cslab4b"));
+				addresses.add(InetAddress.getByName("cslab4c"));
+				addresses.add(InetAddress.getByName("cslab4d"));
+				addresses.add(InetAddress.getByName("cslab4e"));
+				addresses.add(InetAddress.getByName("cslab4f"));
+				addresses.add(InetAddress.getByName("cslab4g"));
+				addresses.add(InetAddress.getByName("cslab4h"));
 				addresses.add(InetAddress.getByName("cslab3a"));
 				addresses.add(InetAddress.getByName("cslab3b"));
 				addresses.add(InetAddress.getByName("cslab3c"));
